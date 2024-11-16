1b3b533f01ed857c74d4c4408c65e458a2dc2aaa
Kyazs <santosjohncasper@gmail.com>
2024-11-16 11:48:58 +0800

    fixed: session msg moved to components

--- public/js/function.js
+++ public/js/function.js
@@ -1,48 +1,3 @@
-// document.getElementById("user-search").addEventListener("input", function () {
-//     const query = this.value;
-//     const suggestions = document.getElementById("suggestions");
-
-//     // Hide suggestions if query is too short
-//     if (query.length < 2) {
-//         suggestions.innerHTML = "";
-//         suggestions.classList.add("hidden");
-//         return;
-//     }
-
-//     // Fetch suggestions from the server
-//     fetch(`{{ route('user.suggestions') }}?query=${query}`)
-//         .then((response) => response.json())
-//         .then((data) => {
-//             suggestions.innerHTML = "";
-
-//             if (data.length > 0) {
-//                 suggestions.classList.remove("hidden");
-//                 data.forEach((user) => {
-//                     const suggestionItem = document.createElement("div");
-//                     suggestionItem.classList.add(
-//                         "p-2",
-//                         "hover:bg-gray-100",
-//                         "cursor-pointer"
-//                     );
-//                     suggestionItem.textContent = user.name;
-
-//                     // Handle click on suggestion
-//                     suggestionItem.addEventListener("click", () => {
-//                         document.getElementById("user-search").value =
-//                             user.name;
-//                         suggestions.innerHTML = "";
-//                         suggestions.classList.add("hidden");
-//                     });
-
-//                     suggestions.appendChild(suggestionItem);
-//                 });
-//             } else {
-//                 suggestions.classList.add("hidden");
-//             }
-//         })
-//         .catch((error) => console.error("Error fetching suggestions:", error));
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         const sessionMessage = document.getElementById("session-message");
@@ -50,4 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
             sessionMessage.style.display = "none";
         }
     }, 2000);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownButton = document.getElementById("dropdownDefaultButton");
+    const dropdownMenu = document.getElementById("dropdown");
+    dropdownButton.addEventListener("click", function () {
+        dropdownMenu.classList.toggle("hidden");
+    });
 });
