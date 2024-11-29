8644d9e1fd977a13f7d8bfca2a15229cb8830b87
Kyazs <santosjohncasper@gmail.com>
2024-11-29 08:33:11 +0800

    added notify for reset password

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class Visitor extends Authenticatable
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Notifiable;
     
     protected $fillable = [
         // List all fillable columns here (for security)
